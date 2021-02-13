865f9b1d86a88e79975b449999660489f962fb7c
istiyakanonto <istiyak.anonto@gmail.com>
2021-02-14 01:15:05 +0600

    final

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const searchSongs = async () => {
     const searchText = document.getElementById('search-field').value
     const url = ` https://api.lyrics.ovh/suggest/${searchText}`
     try {
+        toggleSpinner();
         const res = await fetch(url)
         const data = await res.json()
         displaySongs(data.data)
@@ -29,11 +30,13 @@ const displaySongs = songs => {
         <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
     </div>`
         songContainer.appendChild(songDiv);
+        toggleSpinner();
     })
 }
 const getLyric = async (artist, title) => {
     const url = ` https://api.lyrics.ovh/v1/${artist}/${title}`
     try {
+        toggleSpinner()
         const res = await fetch(url)
         const data = await res.json()
         displayLyrics(data.lyrics)
@@ -45,8 +48,35 @@ const getLyric = async (artist, title) => {
 const displayLyrics = lyrics => {
     const lyricsDiv = document.getElementById('song-lyrics')
     lyricsDiv.innerText = lyrics;
+    toggleSpinner()
 }
 const displayError = error => {
     const errorTag = document.getElementById('error-text')
     errorTag.innerText = error;
 }
+
+const toggleSpinner=()=>
+{
+    const loadingSpinner=document.getElementById('loading-spinner');
+    const song=document.getElementById('song-container');
+    const lyric=document.getElementById('lyric-song');
+   
+        loadingSpinner.classList.toggle('d-none');
+        song.classList.toggle('d-none');
+        lyric.classList.toggle('d-none');
+    
+   
+   
+}
+
+
+
+
+document.getElementById('search-field').addEventListener('keypress',function(event){
+   // event.preventDefault();
+    if(event.key==='Enter')
+    {
+        document.getElementById('search-button').click();
+    }
+})
+
